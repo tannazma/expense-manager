14be2505837c59342b9931c10671b7713c4a8928
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-22 22:33:09 +0100

    created a server

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const port = 3001;
+const prisma = new PrismaClient();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`⚡ Server listening on port: ${port}`);
+});
