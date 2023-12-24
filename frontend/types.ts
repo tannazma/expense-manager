4f2072e9b14a59a45ac40aa42fef1dd9d7b22f97
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-24 18:32:51 +0100

    created interfaces

---
+++ frontend/types.ts
@@ -0,0 +1,39 @@
+export interface ExpenseCategory {
+  id: number;
+  name: string;
+  expenses: Expense[];
+}
+
+export interface Expense {
+  id: number;
+  amount: number;
+  expenseCategoryId: number;
+  expenseCategory: ExpenseCategory;
+  User?: User;
+  userId?: number;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  incomes: Income[];
+  expenses: Expense[];
+}
+
+export interface Income {
+  id: number;
+  amount: number;
+  date: Date;
+  userId: number;
+  user: User;
+  incomeCategoryId: number;
+  incomeCategory: IncomeCategory;
+}
+
+export interface IncomeCategory {
+  id: number;
+  name: string;
+  incomes: Income[];
+}
