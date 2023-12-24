51deb5238af48b030d50efbf65e3cb42b79c34bb
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-24 18:33:25 +0100

    fetch /expenses to get all amount expenses and the categories

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,29 @@
-import expenses from "../../../backend/prisma/data/expenses.json";
-import expensesCategory from "../../../backend/prisma/data/expenseCategories.json";
+import { useEffect, useState } from "react";
+import { Expense } from "../../types";
 
 export default function Home() {
+  const [allExpenses, setAllExpenses] = useState<Expense[]>([]);
+
+  useEffect(() => {
+    const getAllExpenses = async () => {
+      const response = await fetch("http://localhost:3001/expenses");
+      const data = await response.json();
+      setAllExpenses(data);
+    };
+    getAllExpenses();
+  }, []);
+
   return (
     <div>
       <h1>Expenses</h1>
       <div>
-        {expenses.map((expense) => (
-          <span key={expense.id}>{expense.amount}</span>
+        {allExpenses.map((expense) => (
+          <div key={expense.id}>
+            <p key={expense.id}>
+              {expense.amount}
+              <span>{expense.expenseCategory.name}</span>
+            </p>
+          </div>
         ))}
       </div>
     </div>
