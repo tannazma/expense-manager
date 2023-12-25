856222e062ae3a9072f97aec3f533601f9ae2fff
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-25 20:19:28 +0100

    return incomes in home page and added + button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
-import { Expense } from "../../types";
+import { Expense, Income } from "../../types";
 import CreateExpense from "./components/CreateExpense";
+import CreateIncome from "./components/CreateIncome";
 
 export default function Home() {
   const [allExpenses, setAllExpenses] = useState<Expense[]>([]);
-  const [showDialog, setShowDialog] = useState(false);
-  const toggleShowDialog = () => {
-    setShowDialog(!showDialog);
+  const [allIncomes, setAllIncomes] = useState<Income[]>([]);
+  const [showCreateExpenseDialog, setShowCreateExpenseDialog] = useState(false);
+  const [showCreateIncomeDialog, setShowCreateIncomeDialog] = useState(false);
+
+  const toggleShowExpenseDialog = () => {
+    setShowCreateExpenseDialog(!showCreateExpenseDialog);
+  };
+
+  const toggleShowIncomeDialog = () => {
+    setShowCreateIncomeDialog(!showCreateIncomeDialog);
   };
 
   useEffect(() => {
@@ -18,23 +26,62 @@ export default function Home() {
     getAllExpenses();
   }, []);
 
+  useEffect(() => {
+    const getAllIncomes = async () => {
+      const response = await fetch("http://localhost:3001/incomes");
+      const data = await response.json();
+      setAllIncomes(data);
+    };
+    getAllIncomes();
+  }, []);
+
   return (
     <div>
-      <h1>Expenses</h1>
-      <button onClick={toggleShowDialog}> + </button>
-      {showDialog && (
-        <CreateExpense showDialog={showDialog} setShowDialog={setShowDialog} />
-      )}
       <div>
-        {allExpenses.map((expense) => (
-          <div key={expense.id}>
-            <p key={expense.id}>
-              <span>{expense.expenseCategory.icon}</span>
-              {expense.amount}
-              <span>{expense.expenseCategory.name}</span>
-            </p>
-          </div>
-        ))}
+        <div>
+          <h2>Expenses</h2>
+          <button onClick={toggleShowExpenseDialog}> + </button>
+          {showCreateExpenseDialog && (
+            <CreateExpense
+              showDialog={showCreateExpenseDialog}
+              setShowDialog={setShowCreateExpenseDialog}
+            />
+          )}
+        </div>
+        <div>
+          {allExpenses.map((expense) => (
+            <div key={expense.id}>
+              <p key={expense.id}>
+                <span>{expense.expenseCategory.icon}</span>
+                {expense.amount}
+                <span>{expense.expenseCategory.name}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Incomes</h2>
+          <button onClick={toggleShowIncomeDialog}> + </button>
+          {showCreateIncomeDialog && (
+            <CreateIncome
+              showDialog={showCreateIncomeDialog}
+              setShowDialog={setShowCreateIncomeDialog}
+            />
+          )}
+        </div>
+        <div>
+          {allIncomes.map((income) => (
+            <div key={income.id}>
+              <p key={income.id}>
+                <span>{income.incomeCategory.icon}</span>
+                {income.amount}
+                <span>{income.incomeCategory.name}</span>
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
