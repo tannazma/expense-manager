550ca252a84f57620358f4e8340f4b633e74b771
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-28 00:41:35 +0100

    created pie + bar charts for expense

--- frontend/src/pages/components/ExpenseComponent.tsx
+++ frontend/src/pages/components/ExpenseComponent.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import CreateExpense from "../components/CreateExpense";
 import { Expense, ExpenseCategory } from "../../../types";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, Legend, LabelList } from "recharts";
 
 const ExpenseComponent = () => {
   const [allExpenses, setAllExpenses] = useState<Expense[]>([]);
@@ -9,10 +11,20 @@ const ExpenseComponent = () => {
     []
   );
   const [showCreateExpenseDialog, setShowCreateExpenseDialog] = useState(false);
-
+  const [chartData, setChartData] = useState([]);
+  const COLORS = [
+    "#6a0dad",
+    "#9370DB",
+    "#9932CC",
+    "#BA55D3",
+    "#DA70D6",
+    "#EE82EE",
+    "#DDA0DD",
+  ];
   const toggleShowExpenseDialog = () => {
     setShowCreateExpenseDialog(!showCreateExpenseDialog);
   };
+
   useEffect(() => {
     const getAllExpenses = async () => {
       const response = await fetch("http://localhost:3001/expenses");
@@ -46,11 +58,63 @@ const ExpenseComponent = () => {
     getExpensesCategories();
   }, []);
 
+  useEffect(() => {
+    const getExpensesCategories = async () => {
+      const response = await fetch("http://localhost:3001/expense-categories");
+      const data = await response.json();
+
+      setExpenseCategories(data);
+
+      const getExpenseSum = async () => {
+        const response = await fetch("http://localhost:3001/expenses-sum");
+        const sumData = await response.json();
+
+        // create chart data based on the response
+        const chartData = sumData.map((item) => ({
+          name:
+            data.find((cat) => cat.id === item.expenseCategoryId)?.name ||
+            "Unknown",
+          amount: item.amount,
+        }));
+
+        setExpenseSum(sumData);
+        setChartData(chartData); // set chart data
+      };
+
+      getExpenseSum();
+    };
+
+    getExpensesCategories();
+  }, []);
   return (
     <div className="expense-container">
       <div>
         <h2>Expenses</h2>
         <button onClick={toggleShowExpenseDialog}> + </button>
+        <div>
+          <PieChart width={400} height={450}>
+            <Pie
+              dataKey="amount"
+              data={chartData}
+              cx="50%"
+              cy="50%"
+              outerRadius={110}
+              fill="#00008"
+              label={true}
+              paddingAngle={2}
+              animationDuration={500}
+            >
+              {chartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </div>
         {showCreateExpenseDialog && (
           <CreateExpense
             showDialog={showCreateExpenseDialog}
@@ -77,6 +141,26 @@ const ExpenseComponent = () => {
             </div>
           );
         })}
+        <BarChart
+          width={600}
+          height={300}
+          data={chartData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="5 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="amount" fill="#8884d8">
+            <LabelList dataKey="name" position="bottom" angle={45} top={20} />
+          </Bar>
+        </BarChart>
       </div>
     </div>
   );
