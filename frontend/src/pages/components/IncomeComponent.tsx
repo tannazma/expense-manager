a7c89325340add48f464bdf99ed927c8a5d90077
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-28 13:15:36 +0100

    create bar and pie charts for income categories + the buttons to show the different charts

--- frontend/src/pages/components/IncomeComponent.tsx
+++ frontend/src/pages/components/IncomeComponent.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Income, IncomeCategory } from "../../../types";
 import CreateIncome from "../components/CreateIncome";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, Legend, LabelList } from "recharts";
 
 const IncomeComponent = () => {
   const [allIncomes, setAllIncomes] = useState<Income[]>([]);
@@ -10,11 +12,21 @@ const IncomeComponent = () => {
     []
   );
   const [showCreateIncomeDialog, setShowCreateIncomeDialog] = useState(false);
+  const [chartData, setChartData] = useState([]);
+  const [chartType, setChartType] = useState("pie");
 
   const toggleShowIncomeDialog = () => {
     setShowCreateIncomeDialog(!showCreateIncomeDialog);
   };
-
+  const COLORS = [
+    "#6a0dad",
+    "#9370DB",
+    "#9932CC",
+    "#BA55D3",
+    "#DA70D6",
+    "#EE82EE",
+    "#DDA0DD",
+  ];
   useEffect(() => {
     const getAllIncomes = async () => {
       const response = await fetch("http://localhost:3001/incomes");
@@ -49,11 +61,68 @@ const IncomeComponent = () => {
 
   // console.log(sumAllIncomesAmount);
 
+  useEffect(() => {
+    const getIncomesCategories = async () => {
+      const response = await fetch("http://localhost:3001/income-categories");
+      const data = await response.json();
+
+      setIncomeCategories(data);
+
+      const getIncomeSum = async () => {
+        const response = await fetch("http://localhost:3001/incomes-sum");
+        const sumData = await response.json();
+
+        // create chart data based on the response
+        const chartData = sumData.map((item) => ({
+          name:
+            data.find((cat) => cat.id === item.incomeCategoryId)?.name ||
+            "Unknown",
+          amount: item.amount,
+        }));
+
+        setIncomeSum(sumData);
+        setChartData(chartData); // set chart data
+      };
+
+      getIncomeSum();
+    };
+
+    getIncomesCategories();
+  }, []);
+
   return (
     <div className="income-container">
       <div>
         <h2>Incomes</h2>
         <button onClick={toggleShowIncomeDialog}> + </button>
+        <button onClick={() => setChartType("pie")}> Pie Chart </button>
+        <button onClick={() => setChartType("bar")}>s Bar Chart </button>
+        {chartType === "pie" && (
+          <div>
+            <PieChart width={400} height={400}>
+              <Pie
+                dataKey="amount"
+                data={chartData}
+                cx="50%"
+                cy="50%"
+                outerRadius={110}
+                fill="#00008"
+                label={true}
+                paddingAngle={2}
+                animationDuration={1000}
+              >
+                {chartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </div>
+        )}
         {showCreateIncomeDialog && (
           <CreateIncome
             showDialog={showCreateIncomeDialog}
@@ -62,6 +131,46 @@ const IncomeComponent = () => {
         )}
       </div>
       <div className="income-container">
+        {chartType === "bar" && (
+          <BarChart
+            width={400}
+            height={400}
+            data={chartData}
+            // margin={{
+            //   top: 5,
+            //   right: 30,
+            //   left: 20,
+            //   bottom: 5,
+            // }}
+          >
+            <CartesianGrid strokeDasharray="5 3" />
+            <XAxis
+              dataKey="name"
+              angle={-45}
+              textAnchor="end"
+              interval={0}
+              tick={{ fontSize: 15 }}
+              height={90}
+            />
+            <YAxis
+              dataKey="amount"
+              textAnchor="end"
+              interval={0}
+              tick={{ fontSize: 14 }}
+              height={90}
+            />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="amount" animationDuration={1000}>
+              {chartData.map((entry, index) => (
+                <Cell
+                  fill={COLORS[index % COLORS.length]}
+                  key={`cell-${index}`}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        )}
         {incomeSum.map((summary) => {
           const incCategory = incomeCategories.find(
             (cat) => cat.id === summary.incomeCategoryId
