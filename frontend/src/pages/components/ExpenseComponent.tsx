ff0fdd9e69fc7d213295d3c79151621806628e1b
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-29 14:07:42 +0100

    add styles to buttons in expense and inxome components with tailwind

--- frontend/src/pages/components/ExpenseComponent.tsx
+++ frontend/src/pages/components/ExpenseComponent.tsx
@@ -104,9 +104,24 @@ const ExpenseComponent = () => {
     <div className="expense-container">
       <div>
         <h2>Expenses</h2>
-        <button onClick={toggleShowExpenseDialog}> + </button>
-        <button onClick={() => setChartType("pie")}> Pie Chart </button>
-        <button onClick={() => setChartType("bar")}>s Bar Chart </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={toggleShowExpenseDialog}
+        >
+          +
+        </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={() => setChartType("pie")}
+        >
+          Pie Chart
+        </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={() => setChartType("bar")}
+        >
+          s Bar Chart
+        </button>
         {isRendered && chartType === "pie" && (
           <div>
             <PieChart width={400} height={400}>
--- frontend/src/pages/components/IncomeComponent.tsx
+++ frontend/src/pages/components/IncomeComponent.tsx
@@ -110,9 +110,24 @@ const IncomeComponent = () => {
     <div className="income-container">
       <div>
         <h2>Incomes</h2>
-        <button onClick={toggleShowIncomeDialog}> + </button>
-        <button onClick={() => setChartType("pie")}> Pie Chart </button>
-        <button onClick={() => setChartType("bar")}>s Bar Chart </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={toggleShowIncomeDialog}
+        >
+          +
+        </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={() => setChartType("pie")}
+        >
+          Pie Chart
+        </button>
+        <button
+          className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2"
+          onClick={() => setChartType("bar")}
+        >
+          s Bar Chart
+        </button>
         {isRendered && chartType === "pie" && (
           <div>
             <PieChart width={400} height={400}>
