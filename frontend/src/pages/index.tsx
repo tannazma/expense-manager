e9e804b5a643e6345347da9a921e9eba6e231df2
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-31 15:49:17 +0100

    create buttons for accounts

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,32 @@
+import { useContext, useState } from "react";
 import ExpenseComponent from "./components/ExpenseComponent";
 import IncomeComponent from "./components/IncomeComponent";
+import { SelectedAccountContext } from "./components/SelectedAccountContext";
+import { useFetchAccounts } from "./hooks/useFetchAccounts";
 
 export default function Home() {
- 
+  const [selectedAccountId, setSelectedAccount] = useState(0);
+  const accounts = useFetchAccounts();
+
   return (
-    <div className="expense-income-container">
-      <ExpenseComponent />
-      <IncomeComponent />
+    <div>
+      <div className="flex gap-6">
+        {accounts?.map((account) => (
+          <button
+            key={account.id}
+            onClick={() => setSelectedAccount(account.id)}
+          >
+            {account.name}
+          </button>
+        ))}
+        <button onClick={() => setSelectedAccount(0)}>All</button>
+      </div>
+      <div className="expense-income-container">
+        <SelectedAccountContext.Provider value={selectedAccountId}>
+          <ExpenseComponent />
+          <IncomeComponent />
+        </SelectedAccountContext.Provider>
+      </div>
     </div>
   );
 }
