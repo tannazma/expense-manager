b55930dccd38475a020959411898910af12583ce
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-01 15:41:13 +0100

    define a custome hook for ftehc the li=oggedin user with Authorization including token in headers and setUser when the response is ok

---
+++ frontend/src/pages/hooks/useFetchUser.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { User } from "../../../types";
+
+export const useFetchUser = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetch("http://localhost:3001/user", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data);
+      }
+    };
+    getUser();
+  }, []);
+  return user;
+};
