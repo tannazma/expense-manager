58a0f575928d7f418c52c49bb2f8b9f53f94d417
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-01 16:06:12 +0100

    min 3 characters in username on login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -6,7 +6,7 @@ import { z } from "zod";
 import { useFetchUser } from "./hooks/useFetchUser";
 
 const formValuesSchema = z.object({
-  username: z.string().min(5),
+  username: z.string().min(3),
   password: z.string().min(3),
 });
 
