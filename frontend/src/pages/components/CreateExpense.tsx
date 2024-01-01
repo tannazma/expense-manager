ee9b2a7e8004be9654436d03075452423a0f2aaf
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-01 22:44:04 +0100

    add new category button to add expense category name and icon

--- frontend/src/pages/components/CreateExpense.tsx
+++ frontend/src/pages/components/CreateExpense.tsx
@@ -18,6 +18,7 @@ export default function CreateExpense({
   const [accountId, setAccountId] = useState("");
   const [amount, setAmount] = useState("");
   const [expenseCategoryId, setExpenseCategoryId] = useState("");
+  const [isAddingCategory, setIsAddingCategory] = useState(false);
   const [details, setDetails] = useState("");
   const [date, setDate] = useState("");
 
@@ -57,6 +58,22 @@ export default function CreateExpense({
     setShowDialog(!showDialog);
   }
 
+  const [newCategoryName, setNewCategoryName] = useState("");
+  const [newCategoryIcon, setNewCategoryIcon] = useState("💟");
+
+  async function addNewCategory() {
+    console.log({ newCategoryName, newCategoryIcon });
+    fetch("http://localhost:3001/expenses-categories", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: newCategoryName,
+        icon: newCategoryIcon,
+      }),
+    });
+    setIsAddingCategory(false);
+  }
+
   return (
     <div
       className="dialog-backdrop h-screen	w-screen grid place-items-center fixed top-0 left-0 right-0 bottom-0 transition-all-1s z-50 bg-black bg-opacity-50"
@@ -78,24 +95,61 @@ export default function CreateExpense({
             className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
           />
         </label>
-        <label>
-          Category:
-          <select
-            id="category"
-            name="category"
-            value={expenseCategoryId}
-            onChange={(e) => setExpenseCategoryId(e.target.value)}
+        {!isAddingCategory ? (
+          <div className="flex gap-2 bc-white">
+            <label>
+              Category:
+              <select
+                id="category"
+                name="category"
+                value={expenseCategoryId}
+                onChange={(e) => setExpenseCategoryId(e.target.value)}
                 className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
-          >
-            {expenseCategories &&
-              expenseCategories.map((expenseCat) => (
-                <option key={expenseCat.id} value={expenseCat.id}>
-                  {expenseCat.icon}
-                  {expenseCat.name}
-                </option>
-              ))}
-          </select>
-        </label>
+              >
+                {expenseCategories &&
+                  expenseCategories.map((expenseCat) => (
+                    <option key={expenseCat.id} value={expenseCat.id}>
+                      {expenseCat.icon}
+                      {expenseCat.name}
+                    </option>
+                  ))}
+              </select>
+            </label>
+            <button
+              className="bg-purple-500 hover:bg-purple-700 font-bold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded ml-3 text-white"
+              title="Add a new category"
+              onClick={() => setIsAddingCategory(true)}
+            >
+              +
+            </button>
+          </div>
+        ) : (
+          <div className="border flex-col flex">
+            <label>
+              Category Name:
+              <input
+                value={newCategoryName}
+                onInput={(e) => setNewCategoryName(e.target.value)}
+                className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+              />
+            </label>
+            <label>
+              Category Icon
+              <select
+                value={newCategoryIcon}
+                onChange={(e) => setNewCategoryIcon(e.target.value)}
+                className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
+              >
+                {["💟", "👎", "🦷", "👨‍🦲"].map((emoji, index) => (
+                  <option key={index} value={emoji}>
+                    {emoji}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button onClick={addNewCategory}>Save Category</button>
+          </div>
+        )}
         <label>
           Account:
           <select
@@ -135,6 +189,7 @@ export default function CreateExpense({
         <button
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
           type="submit"
+          disabled={isAddingCategory}
         >
           submit
         </button>
