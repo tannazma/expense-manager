ca1050e3d965de7517506592a56ed3e0b0a5534b
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-01 16:01:13 +0100

    if the user is loggedin show hello username on the top in home page, getting user from useFetchUser custom hook

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,18 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import ExpenseComponent from "./components/ExpenseComponent";
 import IncomeComponent from "./components/IncomeComponent";
 import { SelectedAccountContext } from "./components/SelectedAccountContext";
 import { useFetchAccounts } from "./hooks/useFetchAccounts";
+import { useFetchUser } from "./hooks/useFetchUser";
 
 export default function Home() {
+  const user = useFetchUser();
   const [selectedAccountId, setSelectedAccount] = useState(0);
   const accounts = useFetchAccounts();
 
   return (
     <div>
+      {user && <h1>Hello {user.username}</h1>}
       <div className="flex gap-6">
         {accounts?.map((account) => (
           <button
