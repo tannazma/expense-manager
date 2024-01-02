1eda9b146e9f69937d628dec84def812449f07b8
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-02 11:38:42 +0100

    if user and token is logged in return Hello username in the navbar

--- frontend/src/pages/components/NavBar.tsx
+++ frontend/src/pages/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useToken } from "../hooks/useToken";
+import { useFetchUser } from "../hooks/useFetchUser";
 
 const NavBar = () => {
+  const user = useFetchUser();
   const token = useToken();
   const router = useRouter();
 
@@ -20,25 +22,32 @@ const NavBar = () => {
         </h1>
       </Link>
       <div className="pr-8">
-        {token && <p className="mb-3">Hello</p>}
-        <Link
-          href="/register"
-          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Register
-        </Link>
-        {token ? (
-          <a
-            href="/logout"
-            className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2 align-right"
-            id="logout-btn"
-            onClick={handleLogout}
-          >
-            Logout
-          </a>
+        {token && user ? (
+          <div>
+            <div className="mb-3">Hello {user.username}</div>
+            <Link
+              href="/logout"
+              className=" bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 roundedbg-violet-200s  hover:text-white border border-purple-500 hover:border-transparent rounded align-right"
+              id="logout-btn"
+              onClick={handleLogout}
+            >
+              Logout
+            </Link>
+          </div>
         ) : (
-          <div className="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2 align-right">
-            <Link href="/login">Login</Link>
+          <div className=" flex flex-row justify-between items-center gap-3 ">
+            <Link
+              href="/login"
+              className="bg-violet-200 hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded m-2 align-right"
+            >
+              Login
+            </Link>
+            <Link
+              href="/register"
+              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Register
+            </Link>
           </div>
         )}
       </div>
