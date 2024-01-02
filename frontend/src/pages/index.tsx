2bc5c5c01a8be3ad8c0b61b8c8e9ca8c25f9b498
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-02 13:24:19 +0100

    remove hello username p in homw page (It already exists in navbar component)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,18 +3,18 @@ import ExpenseComponent from "./components/ExpenseComponent";
 import IncomeComponent from "./components/IncomeComponent";
 import { SelectedAccountContext } from "./components/SelectedAccountContext";
 import { useFetchAccounts } from "./hooks/useFetchAccounts";
-import { useFetchUser } from "./hooks/useFetchUser";
+// import { useFetchUser } from "./hooks/useFetchUser";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
-  const user = useFetchUser();
+  // const user = useFetchUser();
   const [selectedAccountId, setSelectedAccount] = useState(0);
   const accounts = useFetchAccounts();
 
   return (
     <div>
       <NavBar />
-      {user && <h1>Hello {user.username}</h1>}
+      {/* {user && <h1>Hello {user.username}</h1>} */}
       <div className="flex gap-6">
         {accounts?.map((account) => (
           <button
