67522ad33e75ee58460fc6a09f0317f184b3999c
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-04 12:24:49 +0100

    make creating accounts refresh automatically with refetchAccounts function

--- frontend/src/pages/hooks/useFetchAccounts.ts
+++ frontend/src/pages/hooks/useFetchAccounts.ts
@@ -4,18 +4,20 @@ import { Account } from "../../../types";
 const useFetchAccounts = () => {
   const [accounts, setAccounts] = useState<Account[] | null>(null);
 
+  async function refetchAccounts() {
+    const response = await fetch("http://localhost:3001/accounts", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+    const data = await response.json();
+    setAccounts(data);
+  }
+
   useEffect(() => {
-    const getAllAccounts = async () => {
-      const response = await fetch("http://localhost:3001/accounts", {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      });
-      const data = await response.json();
-      setAccounts(data);
-    };
-    getAllAccounts();
+    refetchAccounts();
   }, []);
-  return accounts;
+
+  return { accounts, refetchAccounts };
 };
 export default useFetchAccounts;
