6ca48ae36e1fb0ac7896345dfe9a3fb41e4ec101
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-09 23:15:27 +0100

    create secondary button component

--- frontend/src/components/SecondaryButton.tsx
+++ frontend/src/components/SecondaryButton.tsx
@@ -1,29 +1,36 @@
-import { useContext } from "react";
+import { useContext, ReactNode } from "react";
 import ThemeContext from "./ThemeContext";
 
-const SecondaryButton = ({ children, onClick }) => {
+type SecondaryButtonProp = {
+  onClick?: (...args: any) => any;
+  children: ReactNode;
+  type?: "submit";
+};
+
+const SecondaryButton = ({ type, children, onClick }: SecondaryButtonProp) => {
   const { theme } = useContext(ThemeContext);
 
-  let secondaryBackgroundColorClass = `text-purple-700 hover:bg-purple-500 hover:bg-purple-500 border-purple-500`;
+  let secondaryBackgroundColorClass = `bg-purple-100 text-purple-700 hover:bg-purple-500 hover:bg-purple-500 border-purple-500`;
 
   if (theme === "red") {
     secondaryBackgroundColorClass =
-      "text-red-700 hover:bg-red-500 hover:bg-red-500 border-red-500";
+      "bg-red-100 text-red-700 hover:bg-red-500 hover:bg-red-500 border-red-500";
   } else if (theme === "green") {
     secondaryBackgroundColorClass =
-      "text-green-700 hover:bg-green-500 hover:bg-green-500 border-green-500";
+      "bg-green-100 text-green-700 hover:bg-green-500 hover:bg-green-500 border-green-500";
   } else if (theme === "blue") {
     secondaryBackgroundColorClass =
-      "text-blue-700 hover:bg-blue-500 hover:bg-blue-500 border-blue-500";
+      "bg-blue-100 text-blue-700 hover:bg-blue-500 hover:bg-blue-500 border-blue-500";
   } else if (theme === "dark") {
     secondaryBackgroundColorClass =
-      "text-white hover:bg-gray-500 hover:bg-gray-500 border-gray-500";
+      "bg-gray-100 text-gray-900 hover:bg-black hover:bg-gray-500 border-gray-500";
   }
 
   return (
     <button
-      className={`${secondaryBackgroundColorClass} text-xs items-center flex gap-2 bg-transparent font-semibold hover:text-white py-1 px-2 border hover:border-transparent rounded m-2`}
+      className={`${secondaryBackgroundColorClass} text-xs items-center flex gap-2 font-semibold hover:text-white py-1 px-2 border hover:border-transparent rounded m-2`}
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
