7a7211cfd56e24a7e70232146d3c0cc01f51d7e0
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-09 23:15:07 +0100

    create primary button component

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -1,9 +1,16 @@
-import { useContext } from "react";
+import { useContext, ReactNode } from "react";
 import ThemeContext from "./ThemeContext";
 
-const PrimaryButton = ({ onClick, children }) => {
+type PrimaryButtonProp = {
+  onClick?: (...args: any) => any;
+  children: ReactNode;
+  type?: "submit";
+};
+
+const PrimaryButton = ({ onClick, children, type }: PrimaryButtonProp) => {
   const { theme } = useContext(ThemeContext);
   let primaryBackgroundColorClass = `bg-purple-500 hover:bg-purple-800 bg-purple-700`;
+  
   if (theme === "red") {
     primaryBackgroundColorClass = "bg-red-500 hover:bg-red-800 border-red-500";
   } else if (theme === "green") {
@@ -14,12 +21,14 @@ const PrimaryButton = ({ onClick, children }) => {
       "bg-blue-500 hover:bg-blue-800 border-blue-500";
   } else if (theme === "dark") {
     primaryBackgroundColorClass =
-      "bg-gray-500 hover:bg-gray-800 border-gray-200";
+      "bg-gray-800 hover:bg-gray-200 border-gray-200";
   }
+
   return (
     <button
-      className={`${primaryBackgroundColorClass} text-xs text-white font-semibold hover:text-white py-2 px-4 border  hover:border-transparent rounded m-2 align-right`}
+      className={`${primaryBackgroundColorClass} text-xs text-white font-semibold hover:text-white py-1 px-2 border hover:border-transparent rounded m-2 align-right`}
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
