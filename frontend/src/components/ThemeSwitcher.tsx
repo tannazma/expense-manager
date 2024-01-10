605796a429444e4590e41ce4c56b08b65879a87b
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-10 01:58:00 +0100

    themeswitcher

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -9,12 +9,14 @@ export function ThemeSwitcher() {
   }
 
   return (
-    <select value={theme} onChange={handleThemeChange}>
-      <option value="default">Default</option>
-      <option value="dark">Dark</option>
-      <option value="blue">Blue</option>
-      <option value="red">Red</option>
-      <option value="green">Green</option>
-    </select>
+    <div className="text-xs flex-1 items-center m-3">
+      <select value={theme} onChange={handleThemeChange} className="p-1 rounded bg-white">
+        <option value="default">Default</option>
+        <option value="dark">Dark</option>
+        <option value="blue">Blue</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+      </select>
+    </div>
   );
 }
