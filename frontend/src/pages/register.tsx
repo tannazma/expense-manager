cc6f4f1c4dd0a6c5aced7aa5812788f2e4d886e9
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-10 01:56:00 +0100

    theme color for register page

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import NavBar from "../components/NavBar";
+import { useContext } from "react";
+import ThemeContext from "@/components/ThemeContext";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const dataFromFormValidator = z.object({
   username: z.string().min(4),
@@ -39,11 +42,37 @@ const Register = () => {
     }
   };
 
+  const { theme } = useContext(ThemeContext);
+  let registerButton = "bg-purple-500 hover:bg-purple-700";
+  let registerBackgroundColor = "bg-violet-200";
+  let registerBackgroundColor2 = "bg-violet-100";
+  if (theme === "red") {
+    registerButton = "bg-red-500 hover:bg-red-700";
+    registerBackgroundColor = "bg-red-200";
+    registerBackgroundColor2 = "bg-red-100";
+  } else if (theme === "green") {
+    registerButton = "bg-green-500 hover:bg-green-700";
+    registerBackgroundColor = "bg-green-200";
+    registerBackgroundColor2 = "bg-green-100";
+  } else if (theme === "blue") {
+    registerButton = "bg-blue-500 hover:bg-blue-700";
+    registerBackgroundColor = "bg-blue-200";
+    registerBackgroundColor2 = "bg-blue-100";
+  } else if (theme === "dark") {
+    registerButton = "bg-gray-700 hover:bg-gray-400 hover:text-black";
+    registerBackgroundColor = "bg-gray-200";
+    registerBackgroundColor2 = "bg-gray-100";
+  }
+
   return (
     <div>
       <NavBar />
-      <div className="min-h-screen items-center flex justify-center p-20 align-top bg-violet-200">
-        <main className="min-w-[500px] rounded-3xl shadow-xl p-10 bg-violet-100">
+      <div
+        className={`${registerBackgroundColor} min-h-screen items-center flex justify-center p-20 align-top`}
+      >
+        <main
+          className={`${registerBackgroundColor2} min-w-[500px] rounded-3xl shadow-xl p-10 `}
+        >
           <div>
             <h1 className="text-2xl font-bold text-center mb-4 cursor-pointer">
               Register
@@ -88,7 +117,7 @@ const Register = () => {
             )}
             <button
               type="submit"
-              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-10"
+              className={`${registerButton} text-white font-bold py-2 px-4 rounded mt-10`}
             >
               Register
             </button>
