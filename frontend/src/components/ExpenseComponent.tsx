d9b15a09b87a3a115d38de3e0551a0ffaa38e0c0
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-14 21:13:58 +0100

    expenses can filter and return when filter the date

--- frontend/src/components/ExpenseComponent.tsx
+++ frontend/src/components/ExpenseComponent.tsx
@@ -13,13 +13,9 @@ interface expenseProps {
 }
 
 const ExpenseComponent = ({ refetchBalance }: expenseProps) => {
-  const selectedAccountId = useContext(SelectedAccountContext);
   const { dateFilter } = useContext(FilterContext);
-  // console.log(dateFilter);
-  const [dateFilterState, setDateFilterState] = useState({
-    from: "y",
-    to: "ju",
-  });
+  const selectedAccountId = useContext(SelectedAccountContext);
+  const { theme } = useContext(ThemeContext);
   const [expenseSum, setExpenseSum] = useState<expenseSumData[]>([]);
   const [expenseCategories, setExpenseCategories] = useState<ExpenseCategory[]>(
     []
@@ -32,37 +28,65 @@ const ExpenseComponent = ({ refetchBalance }: expenseProps) => {
 
   useEffect(() => {
     const getExpensesCategories = async () => {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVERURL}/expense-categories`
-      );
-      const categories: ExpenseCategory[] = await response.json();
-
-      setExpenseCategories(categories);
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVERURL}/expense-categories`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch expense categories");
+        }
+        const categories: ExpenseCategory[] = await response.json();
+        setExpenseCategories(categories);
+      } catch (error) {
+        console.error(error);
+      }
     };
     getExpensesCategories();
   }, []);
 
   const getExpenseSum = async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVERURL}/accounts/${selectedAccountId}/expenses-sum?from=${dateFilter.from}&to=${dateFilter.to}`,
-      {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
+    if (dateFilter) {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVERURL}/accounts/${selectedAccountId}/expenses-sum-filter?from=${dateFilter.from}&to=${dateFilter.to}`,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch expense sum");
+        }
+        const sumData: expenseSumData[] = await response.json();
+        console.log("sum data with filtering data", sumData);
+        setExpenseSum(sumData);
+      } catch (error) {
+        console.error(error);
       }
-    );
-    const sumData: expenseSumData[] = await response.json();
-    setExpenseSum(sumData);
+    } else {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVERURL}/accounts/${selectedAccountId}/expenses-sum`,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      const sumData: expenseSumData[] = await response.json();
+      console.log("all sumdata before filtering date", sumData);
+      setExpenseSum(sumData);
+    }
   };
+  console.log("efffff", expenseSum);
 
   useEffect(() => {
     getExpenseSum();
   }, [selectedAccountId, dateFilter]);
 
-  const { theme } = useContext(ThemeContext);
+  // Code to set the background color class based on the theme
   let entryBackgroundColorClass =
     "border-purple-500 bg-violet-100 hover:bg-purple-500 text-purple-700";
-
   if (theme === "red") {
     entryBackgroundColorClass =
       "border-red-500 bg-red-100 hover:bg-red-500 text-red-700";
@@ -77,61 +101,58 @@ const ExpenseComponent = ({ refetchBalance }: expenseProps) => {
       "border-gray-500 bg-gray-500 hover:bg-gray-300 text-gray-300";
   }
 
+  // The component renders a list of expense categories and their sums, and a button to create a new expense
   return (
-    <FilterContext.Provider
-      value={{ dateFilter, setDateFilter: setDateFilterState }}
-    >
-      <div className="flex-1 pr-10 pl-10">
-        <div>
-          <h2 className="text-lg font-semibold">Expenses</h2>
-          <ExpenseCharts />
-          <div className="flex justify-end pr-8">
-            <PrimaryButton onClick={toggleShowExpenseDialog}>+</PrimaryButton>
-          </div>
-          {showCreateExpenseDialog && (
-            <CreateEntry
-              showDialog={showCreateExpenseDialog}
-              type="expense"
-              setShowDialog={setShowCreateExpenseDialog}
-              onCreated={getExpenseSum}
-              refetchBalance={refetchBalance}
-            />
-          )}
+    <div className="flex-1 pr-10 pl-10">
+      <div>
+        <h2 className="text-lg font-semibold">Expenses</h2>
+        <ExpenseCharts />
+        <div className="flex justify-end pr-8">
+          <PrimaryButton onClick={toggleShowExpenseDialog}>+</PrimaryButton>
         </div>
-        <div className="flex flex-col pr-10 pl-10">
-          {expenseSum
-            .sort((a, b) => (a.amount > b.amount ? -1 : 1))
-            .map((summary) => {
-              const expCategory = expenseCategories.find(
-                (cat) => cat.id === summary.expenseCategoryId
-              );
-              return (
-                <Link
-                  key={summary.expenseCategoryId}
-                  href={`/expenses/${expCategory?.id}`}
-                  className="text-xs"
-                >
-                  <div
-                    className={`${entryBackgroundColorClass} flex pl-3 items-center font-semibold hover:text-white border hover:border-transparent rounded m-2 px-2 py-2`}
-                  >
-                    <div className="flex justify-between gap-2 items-center">
-                      {expCategory && (
-                        <div className="flex justify-between gap-2 items-center">
-                          <p className="text-xs">{expCategory.icon}</p>
-                          <p className="text-xs">{expCategory.name}</p>
-                        </div>
-                      )}
-                    </div>
-                    <p className="flex flex-1 justify-end pr-6">
-                      {summary.amount} €
-                    </p>
-                  </div>
-                </Link>
-              );
-            })}
-        </div>
+        {showCreateExpenseDialog && (
+          <CreateEntry
+            showDialog={showCreateExpenseDialog}
+            type="expense"
+            setShowDialog={setShowCreateExpenseDialog}
+            onCreated={getExpenseSum}
+            refetchBalance={refetchBalance}
+          />
+        )}
       </div>
-    </FilterContext.Provider>
+      <div className="flex flex-col pr-10 pl-10">
+        {expenseSum
+          .sort((a, b) => (a.amount > b.amount ? -1 : 1))
+          .map((summary) => {
+            const expCategory = expenseCategories.find(
+              (cat) => cat.id === summary.expenseCategoryId
+            );
+            return (
+              <Link
+                key={summary.expenseCategoryId}
+                href={`/expenses/${expCategory?.id}`}
+                className="text-xs"
+              >
+                <div
+                  className={`${entryBackgroundColorClass} flex pl-3 items-center font-semibold hover:text-white border hover:border-transparent rounded m-2 px-2 py-2`}
+                >
+                  <div className="flex justify-between gap-2 items-center">
+                    {expCategory && (
+                      <div className="flex justify-between gap-2 items-center">
+                        <p className="text-xs">{expCategory.icon}</p>
+                        <p className="text-xs">{expCategory.name}</p>
+                      </div>
+                    )}
+                  </div>
+                  <p className="flex flex-1 justify-end pr-6">
+                    {summary.amount} €
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+    </div>
   );
 };
 export default ExpenseComponent;
