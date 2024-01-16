6813aad6c626c10e60cbc19018ba7b3ffbfc7ec6
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-16 14:34:53 +0100

    rename useBalance hook from .tsx -> .ts

---
+++ frontend/src/hooks/useBalance.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { expenseSumData, incomeSumData } from "../../types";
+
+const useBalance = () => {
+  const [expenseSum, setExpenseSum] = useState<expenseSumData[]>([]);
+  const [incomeSum, setIncomeSum] = useState<incomeSumData[]>([]);
+  const [selectedAccountId, setSelectedAccount] = useState(0);
+
+  let incomeTotal = incomeSum.reduce(
+    (acc, curr) => acc + Number(curr.amount),
+    0
+  );
+  let expenseTotal = expenseSum.reduce(
+    (acc, curr) => acc + Number(curr.amount),
+    0
+  );
+  const balance = incomeTotal - expenseTotal;
+
+  const getIncomeSum = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVERURL}/accounts/${selectedAccountId}/incomes-sum`,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    );
+    const sumData: incomeSumData[] = await response.json();
+    setIncomeSum(sumData);
+  };
+
+  useEffect(() => {
+    getIncomeSum();
+  }, [selectedAccountId]);
+
+  const getExpenseSum = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVERURL}/accounts/${selectedAccountId}/expenses-sum`,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    );
+    const sumData: expenseSumData[] = await response.json();
+    setExpenseSum(sumData);
+  };
+
+  useEffect(() => {
+    getExpenseSum();
+  }, [selectedAccountId]);
+
+  function refetchBalance() {
+    getExpenseSum();
+    getIncomeSum();
+  }
+
+  return { balance, selectedAccountId, setSelectedAccount, refetchBalance };
+};
+export default useBalance;
