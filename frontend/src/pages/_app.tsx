cccf3316d5c63e2a31da89b557d58d8edeaa8436
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-19 12:52:48 +0100

    make toasts work

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Toaster />
       <Component {...pageProps} />
     </ThemeProvider>
   );
