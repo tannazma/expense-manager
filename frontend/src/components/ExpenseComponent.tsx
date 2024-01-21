9f59984db8959393b4b0d7242ba7b939685c67cb
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-21 21:25:54 +0100

    changed the route for expenses

--- frontend/src/components/ExpenseComponent.tsx
+++ frontend/src/components/ExpenseComponent.tsx
@@ -129,7 +129,7 @@ const ExpenseComponent = ({ refetchBalance }: expenseProps) => {
             return (
               <Link
                 key={summary.expenseCategoryId}
-                href={`/accounts/${selectedAccountId}/category/${expCategory?.id}/expenses`}
+                href={`/accounts/${selectedAccountId}/expenses/category/${expCategory?.id}`}
                 className="text-xs"
               >
                 <div
