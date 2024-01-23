d636041e3d6f72623fe52cd8a693649a8df229b8
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-23 13:59:34 +0100

    change build command + port to deploy backemd

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ import { hashSync, compareSync } from "bcrypt";
 import bcrypt from "bcrypt";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const prisma = new PrismaClient();
 
 app.use(cors());
