b734eaff853d2e8662d445d70d2c0793fee3ff0e
tannazma <97254921+tannazma@users.noreply.github.com>
2024-01-25 11:00:04 +0100

    fix home page work

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,8 @@ import useFetchUser from "../hooks/useFetchUser";
 import Home from "../components/Home";
 export default function Index() {
   const user = useFetchUser();
-
+  if (!user) {
+    
+  }
   return <div>{user && <Home />}</div>;
 }
